9f418b67b1e1e0eca3a778b1b65b18644f7f690c
jtruelas <joshuatruelas@gmail.com>
2025-01-11 15:15:36 -0800

    Added function to get random selection from computer

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,21 @@
 console.log("Welcome to Rock Paper Scissors!");
+
+function getComputerChoice () {
+    
+    let computerChoice = Math.floor(Math.random() * 3);
+
+    if (computerChoice == 0) {
+        return "Rock";
+    }
+    else if (computerChoice == 1) {
+        return "Paper";
+    }
+    else if (computerChoice == 2) {
+        return "Scissors";
+    }
+    else {
+        return "Error";
+    }
+}
+
+console.log(getComputerChoice());
