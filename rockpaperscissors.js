6226205255b3f97353dac02722d97f8aa0ba4208
jtruelas <joshuatruelas@gmail.com>
2025-01-11 16:26:03 -0800

    Added function for game logic.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,3 @@
-console.log("Welcome to Rock Paper Scissors!");
-
 let humanScore = 0;
 let computerScore = 0;
 
@@ -7,25 +5,85 @@ function getComputerChoice () {
     
     let computerChoice = Math.floor(Math.random() * 3);
 
-    if (computerChoice == 0) {
-        return "Rock";
-    }
-    else if (computerChoice == 1) {
-        return "Paper";
-    }
-    else if (computerChoice == 2) {
-        return "Scissors";
-    }
-    else {
-        return "Error";
-    }
+    // 0 = rock, 1 = paper, 2 = scissors
+
+    return computerChoice;
 }
 
 function getHumanChoice () {
-    let humanChoice = prompt("Pick one, Rock, Paper, Scissors: ");
+    
+    let humanChoice = prompt("Welcome to Rock, Paper, Scissors!\nPick one, Rock, Paper, Scissors: ").toLowerCase();
 
     return humanChoice;
 }
 
-console.log("You chose: ", getHumanChoice());
-console.log("Computer chose: ", getComputerChoice());
+function playRound(humanChoice, computerChoice) {
+
+    // convert computerChoice to text
+    if (computerChoice == 0) {
+        computerChoice = "rock";
+    }
+    else if (computerChoice == 1) {
+        computerChoice = "paper";
+    }
+    else {
+        computerChoice = "scissors";
+    }
+
+    // create end game messages
+    let winMessage = `You win! You chose: ${humanChoice}! I chose: ${computerChoice}!`;
+    let loseMessage = `You lose! You chose: ${humanChoice}! I chose: ${computerChoice}!`;
+    let tieMessage = `It's a tie! You chose: ${humanChoice}! I chose: ${computerChoice}!`;
+
+    // find winner of the round
+    switch(humanChoice) {
+        case "rock":
+            switch(computerChoice) {
+                case "rock":
+                    console.log(tieMessage);
+                    break;
+                case "paper":
+                    console.log(loseMessage);
+                    computerScore += 1;
+                    break;
+                case "scissors":
+                    console.log(winMessage);
+                    humanScore += 1;
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerChoice) {
+                case "rock":
+                    console.log(winMessage);
+                    humanScore += 1;
+                    break;
+                case "paper":
+                    console.log(tieMessage);
+                    break;
+                case "scissors":
+                    console.log(loseMessage);
+                    computerScore += 1;
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerChoice) {
+                case "rock":
+                    console.log(loseMessage);
+                    computerScore += 1;
+                    break;
+                case "paper":
+                    console.log(winMessage);
+                    humanScore += 1;
+                    break;
+                case "scissors":
+                    console.log(tieMessage);
+                    break;
+            }
+            break;
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
+console.log(`Total scores:\nYou: ${humanScore}\nMe: ${computerScore}`);
