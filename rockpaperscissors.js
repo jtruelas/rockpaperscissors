ecf057c713222bc9696bfb73506a7634ba3ba6b9
jtruelas <joshuatruelas@gmail.com>
2025-01-12 20:48:53 -0800

    Updated message to display round results. Updated score board to reflect current score. Added alert when either player or computer reaches final score of 5 and reset window.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,8 +1,12 @@
 let humanScore = 0;
 let computerScore = 0;
 
-function getComputerChoice () {
-    
+// create elements
+const playerScoreContainer = document.getElementById("player-score");
+const compScoreContainer = document.getElementById("computer-score");
+
+function getComputerChoice() {
+
     let computerChoice = Math.floor(Math.random() * 3);
 
     // 0 = rock, 1 = paper, 2 = scissors
@@ -21,6 +25,9 @@ function getHumanChoice () {
 
 function playRound(humanChoice, computerChoice) {
 
+    // create elements
+    const container = document.getElementById("result-message");
+
     // convert computerChoice to text
     if (computerChoice == 0) {
         computerChoice = "rock";
@@ -33,69 +40,88 @@ function playRound(humanChoice, computerChoice) {
     }
 
     // create end game messages
-    let winMessage = `You win! You chose ${humanChoice}! I chose ${computerChoice}!`;
-    let loseMessage = `You lose! You chose ${humanChoice}! I chose ${computerChoice}!`;
-    let tieMessage = `It's a tie! You chose ${humanChoice}! I chose ${computerChoice}!`;
+    let winMessage = `You win!\nYou chose ${humanChoice}!\nI chose ${computerChoice}!`;
+    let loseMessage = `You lose!\nYou chose ${humanChoice}!\nI chose ${computerChoice}!`;
+    let tieMessage = `It's a tie!\nYou chose ${humanChoice}!\nI chose ${computerChoice}!`;
 
     // find winner of the round
-    switch(humanChoice) {
+    switch (humanChoice) {
         case "rock":
-            switch(computerChoice) {
+            switch (computerChoice) {
                 case "rock":
-                    console.log(tieMessage);
+                    container.textContent = tieMessage;
                     break;
                 case "paper":
-                    console.log(loseMessage);
+                    container.textContent = loseMessage;
                     computerScore += 1;
+                    compScoreContainer.textContent = computerScore;
                     break;
                 case "scissors":
-                    console.log(winMessage);
+                    container.textContent = winMessage;
                     humanScore += 1;
+                    playerScoreContainer.textContent = humanScore;
                     break;
             }
             break;
         case "paper":
-            switch(computerChoice) {
+            switch (computerChoice) {
                 case "rock":
-                    console.log(winMessage);
+                    container.textContent = winMessage;
                     humanScore += 1;
+                    playerScoreContainer.textContent = humanScore;
                     break;
                 case "paper":
-                    console.log(tieMessage);
+                    container.textContent = tieMessage;
                     break;
                 case "scissors":
-                    console.log(loseMessage);
+                    container.textContent = loseMessage;
                     computerScore += 1;
+                    compScoreContainer.textContent = computerScore;
                     break;
             }
             break;
         case "scissors":
-            switch(computerChoice) {
+            switch (computerChoice) {
                 case "rock":
-                    console.log(loseMessage);
+                    container.textContent = loseMessage;
                     computerScore += 1;
+                    compScoreContainer.textContent = computerScore;
                     break;
                 case "paper":
-                    console.log(winMessage);
+                    container.textContent = winMessage;
                     humanScore += 1;
+                    playerScoreContainer.textContent = humanScore;
                     break;
                 case "scissors":
-                    console.log(tieMessage);
+                    container.textContent = tieMessage;
                     break;
             }
             break;
     }
-}
 
+    if(humanScore == 5) {
+        setTimeout(() => {
+        alert("YOU WIN!");
+        window.location.reload();
+        }, 100);
+    }
+    if(computerScore == 5) {
+        setTimeout(() => {
+        alert("I WIN!");
+        window.location.reload();
+        }, 100);
+    }
+}
+/* Legacy function prior ui
 function playGame() {
-    
+
     let i = 0;
-    
+
     while (humanScore != 3 && computerScore != 3 && humanScore + computerScore != 5) {
-        
+
         // initiate round
         playRound(getHumanChoice(), getComputerChoice());
-        
+
         // list scores
         console.log(`Total scores:\nYou: ${humanScore}\nMe: ${computerScore}`);
     };
@@ -110,6 +136,7 @@ function playGame() {
         prompt("It's a tie!\n(press enter to continue)");
     }
 }
+*/
 
 // create event listeners
 const btns = document.querySelectorAll("button");
